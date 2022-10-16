71c46e4175f77dc593345f267a25492e5ff7a26c
xyntix <61931770+xyntix@users.noreply.github.com>
2022-10-16 11:31:18 +0700

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -4,12 +4,12 @@ import fetch from 'node-fetch'
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 
-let str = `*https://github.com/ImYanXiao/Elaina-MultiDevice*`
+let str = `*https://github.com/xyntix/Elaina-MultiDevice/e*`
 let wibu = `https://hadi-api.herokuapp.com/api/loli` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, str, wm,'ᴏᴋ','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://Instagram.com/Xiao_yan_21",
-    title: 'Elaina-MultiDevice',
+    mediaUrl: "https://Instagram.com/Reeeee1412",
+    title: 'Secret-MultiDevice',
     body: 'want source code?',
     thumbnail: thumb,
     sourceUrl: sig
