d1a672f245f50f2bb9c5223565581c9cf2d401f4
xyntix <61931770+xyntix@users.noreply.github.com>
2022-10-16 11:29:26 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => {
 let text = `
 ┌─「 Donasi • Pulsa 」
-│ • *Indosat:* [${global.ppulsa}]
+│ • *Pulsa:* [${global.ppulsa}]
 ❏────
 
 ┌─「 Donasi • Non Pulsa 」
